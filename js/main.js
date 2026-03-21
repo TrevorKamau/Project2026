ea2cfa910edb5f1c3cd54c6c23d6231cac526d89
Kamil <G00433270@atu.ie>
2026-03-21 22:01:21 +0000

    Report Lost Pet page UI

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log("PawFind loaded");
+
+//TODO: This will send the form data to the backend later
+function submitForm() {
+    alert("Report has been submitted!");
+}
--- main.js
+++
@@ -1,1 +0,0 @@
-console.log("PawFind loaded");
