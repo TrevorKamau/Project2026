08b0bbf80f8b780cfa0804eca00bc04303c7d58d
Kamil <G00433270@atu.ie>
2026-03-23 16:09:27 +0000

    Register Page UI

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 console.log("PawFind loaded");
 
-//TODO: This will send the form data to the backend later
+// TODO: This will send the form data to the backend later
 function submitForm() {
     alert("Report has been submitted!");
 }
 
-//TODO: Will send login details to the backend later
+// TODO: Will send login details to the backend later
 function handleLogin() {
     alert("Login clicked!");
+}
+
+// TODO: Will send register details to the backend later
+function handleRegister() {
+    alert("Register clicked!");
 }
