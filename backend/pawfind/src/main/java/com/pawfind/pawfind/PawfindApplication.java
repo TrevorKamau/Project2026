0b04cfa33412ad1d467e9730abbd307b2d29e9ee
Kamil <G00433270@atu.ie>
2026-04-10 20:20:42 +0100

    Add Spring Boot

---
+++ backend/pawfind/src/main/java/com/pawfind/pawfind/PawfindApplication.java
@@ -0,0 +1,13 @@
+package com.pawfind.pawfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawfindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawfindApplication.class, args);
+	}
+
+}
---
+++ backend/pawfind/src/test/java/com/pawfind/pawfind/PawfindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawfind.pawfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawfindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
